ada59696fdcc501ae9d363998ec42416f911ed66
wuranran <wuranran@autoforce.net>
2019-04-19 17:43:45 +0800

    事件处理

--- react-threejs-app/src/Menu/menu.jsx
+++ react-threejs-app/src/Menu/menu.jsx
@@ -1,18 +1,68 @@
 
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 import './menu.css';
 
-class Menu extends Component{
-    constructor(props) {
-        super(props);
-      }
- componentDidMount(){
-    
-  }
-  render(){
-      return(
-          <div className="menu-text1">切换场景</div>
-      )
-  }
+const LogoutButton = (props) => {
+     return( <div className="loginout" onClick={props.onClick}>{props.username}</div>)
 }
-export default Menu;
+const LoginButton = (props) => {
+        return( <div className="login" onClick={props.onClick}>{props.age}</div>)
+}
+const WarningBanner =(props)=> {
+    if (!props.warn) {
+      return null;
+    }
+    return (
+      <div className="warning">
+        警告!
+      </div>
+    );
+  }
+  const InputBanner =(props)=> {
+    let hint =props.hint
+    if (!hint) {
+        hint ="请输入数字"
+    }
+    return (
+      <div><input type="number" className="inputbox"  placeholder={hint} onChange ={props.onChange} value={props.value} /></div>
+    );
+  }
+
+  export default  class Menu extends Component{
+     constructor(props) {
+        super(props);
+        this.handleLoginClick = this.handleLoginClick.bind(this);
+        this.handleLogoutClick = this.handleLogoutClick.bind(this);
+        this.handelChange =this.handelChange.bind(this)
+        this.state = { isLoggedIn:true,usename:"wrr",age:"25",showWarning:true,hint:"输入姓名哈",inputval:"12333" };
+      }
+     componentDidMount(){
+    
+     }
+     handleLogoutClick(){
+        this.setState({isLoggedIn: false,age:20,showWarning: !this.state.showWarning}); 
+     }
+     handleLoginClick(){
+        this.setState({isLoggedIn: true,showWarning: !this.state.showWarning});
+    }
+    handelChange(e){
+        this.setState({inputval:e.target.value})
+    }
+     
+    render(){
+        const isLoggedIn = this.state.isLoggedIn;
+        const inputVal =this.state.inputval
+        return(
+            <div>{isLoggedIn ? (
+                <div><LogoutButton onClick={this.handleLogoutClick} username={this.state.usename} />
+                <InputBanner hint ={this.state.hint} onChange={this.handelChange} value={inputVal}/>
+                <WarningBanner warn={this.state.showWarning} /></div>
+              ) : (
+                <LoginButton onClick={this.handleLoginClick}  age ={this.state.age}/>
+              )}
+              </div>
+        )
+    }
+}
+
