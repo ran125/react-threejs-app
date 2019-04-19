34075be011b7a4289e399cc5d2a0ee5409a3df22
wuranran <wuranran@autoforce.net>
2019-04-19 18:35:46 +0800

    分场景父模块,子场景

--- react-threejs-app/src/threejs/ThreeScene/ThreeScene.jsx
+++ react-threejs-app/src/threejs/ThreeScene/ThreeScene.jsx
@@ -8,19 +8,54 @@ const OrbitControls = orbtCtrls(THREE);
 class ThreeScene extends Component{
   constructor(props) {
     super(props);
-    this.name = {date: new Date()};
+    this.setState({scene:null})
   }
   //在组建输出到dom 后会执行componentdidmount() 钩子
   componentDidMount(){
   
- }
+  }
+  initScene(){
+    this.width =document.getElementById('WebGL-output').clientWidth;
+    this.height =document.getElementById('WebGL-output').clientHeight;
+    this.initScene();
+    this.initCamera();
+    this.initRender()
+    this.animate();
+    this.initControls();
+  }
+  initScene() {
+    this.scene = new THREE.Scene();
+   }
+   initCamera() {
+    this.camera = new THREE.PerspectiveCamera( 45, this.width / this.height, 1, 1000000 );
+    this.camera.position.z = 4;
+   }
+   initControls() {
+    this.orbitControls = new OrbitControls(this.camera,this.renderer.domElement);
+    // this.orbitControls.autoRotate = false;
+   }
+   initRender() {
+    let canvas = document.getElementById( 'webgl' );
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true, canvas: canvas
+    });
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize( this.width, this.height );
+   }
+   animate() {
+    requestAnimationFrame(this.animate);
+    this.renderer.render(this.scene, this.camera );
+  }
+
  //组建从dom 中移除会调用componentWillUnmount 钩子函数
  componentWillUnmount(){
   //  _this =null;
  }
- render(props){
+ render(){
    return(
-     <div id='scene'></div>
+     <div id='scene'>
+     <Sphere  scene={this.scene} />
+     </div>
    )
  }
 }
--- react-threejs-app/src/threejs/sphere/ThreeSphere.jsx
+++ react-threejs-app/src/threejs/sphere/ThreeSphere.jsx
@@ -1,3 +1,32 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 import orbtCtrls from 'three-orbit-controls'
+import ThreeScene from '../ThreeScene/ThreeScene'
+
+export default class Sphere extends React{
+    constructor(props) {
+        super(props);
+        console.log(props)
+      }
+      //在组建输出到dom 后会执行componentdidmount() 钩子
+      componentDidMount(){
+      
+      }
+    initRoom(){
+        var materials = [];
+        for ( var i = 0; i < 8; i ++ ) {
+            materials.push( new THREE.MeshBasicMaterial( { color: Math.random() * 0xffffff, side: THREE.BackSide } ) );
+        }
+        var geometry = new THREE.BoxBufferGeometry( 3, 3, 3 );
+      
+        var mesh = new THREE.Mesh( geometry, materials );
+      
+        this.scene.add(mesh);               
+       }
+
+    render(props){
+        console.log(props)
+        // //initRoom
+        return(<div></div>)
+      }
+}
